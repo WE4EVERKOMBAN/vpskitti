0ee91bfd6e51f2887b80ba805bc022bd00d8305a
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-10-09 15:23:48 +0530

    Update and rename data(1).js to data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://radical:vishu@radical.bvk17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5426620875:AAHJf7iODwP4E9TFUhpKDHYw3scIbxT2kKM',
+
+bot_name:'ChickenDropBot',//bot username without @
+
+bot_admin:895687774,
+
+channelsList : ['@AirdropAssasins'],
+
+channelscheck : ['@AirdropAssasins'],
+
+airdropName : 'Chicken Dinner Airdrop',
+
+reffer_bonus:100000,// ral bonus amount
+
+min_wd:300000,//min withdrawal amount
+
+currency:'rdt', //bot cuurency
+
+payment_channel:'@PayOutToken'// payment channel username
+
+}
